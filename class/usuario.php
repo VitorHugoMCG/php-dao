1a9c4d70648e8378919863a7a607e028fcc19435
Vitor Hugo Marcondes <vitor.oficial28@gmail.com>
2025-03-31 19:48:05 -0300

    Criamos a classe usuario

--- class/usuario.php
+++ class/usuario.php
@@ -1,3 +1,76 @@
 <?php
-    
+
+class Usuario {
+
+    private $idusuario;
+    private $deslogin;
+    private $dessenha;
+    private $dtcadastro;
+
+    public function getIdusuario(){
+        return $this->idusuario;
+    }
+
+    public function setIdusuario($value){
+        $this->idusuario = $value;
+    }
+
+    public function getDeslogin(){
+        return $this->deslogin;
+    }
+
+    public function setDeslogin($value){
+        $this->deslogin = $value;
+    }
+
+    public function getDessenha(){
+        return $this->dessenha;
+    }
+
+    public function setDessenha($value){
+        $this->dessenha = $value;
+    }
+
+    public function getDtcadastro(){
+        return $this->dtcadastro;
+    }
+
+    public function setDtcadastro($value){
+        if ($value instanceof DateTime) {
+            $this->dtcadastro = $value;
+        } else {
+            $this->dtcadastro = new DateTime($value);
+        }
+    }
+
+    public function loadById($id){
+
+        $sql = new Sql();
+
+        $result = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(
+            ":ID"=>$id
+        ));
+
+        if (isset($result[0])) { // Corrigido de $results para $result
+            $row = $result[0];
+
+            $this->setIdusuario($row['idusuario']);
+            $this->setDeslogin($row['deslogin']);
+            $this->setDessenha($row['dessenha']);
+            
+            // Corrigido erro de digitação em DateTime
+            $this->setDtcadastro($row['dtcadastro']);
+        }
+    }
+
+    public function __toString(){
+        return json_encode(array(
+            "idusuario"=>$this->getIdusuario(),
+            "deslogin"=>$this->getDeslogin(),
+            "dessenha"=>$this->getDessenha(),
+            "dtcadastro"=>($this->getDtcadastro() instanceof DateTime) ? $this->getDtcadastro()->format("d/m/Y H:i:s") : null
+        ));
+    }
+}
+
 ?>
