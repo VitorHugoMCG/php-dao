58c3b7d5ffd0d196724039a4675db87ced503e96
Vitor Hugo Marcondes <vitor.oficial28@gmail.com>
2025-04-09 16:46:09 -0300

    Metodos de lista

--- index.php
+++ index.php
@@ -2,10 +2,22 @@
 
     require_once("config.php");
    
+    //Carrega 1 usuario
+    //$root = new Usuario();
+    //$root->loadById(3);
+    //echo $root;
 
-    $root = new Usuario();
-    $root->loadById(3);
+    //carrega uma lista de usuarios
+    //$lista = Usuario::getList();
+    //echo json_encode($lista);
 
-    echo $root;
+    //carrega uma lista de usuarios, buscando pelo login
+    //$search = Usuario::search("jo");
+    //echo json_encode($search);
+
+    //carrega um usuario usando o login e a senha
+    $usuario = new Usuario();
+    $usuario->login("vitor","torugo");
+    echo $usuario;
 
 ?>
